372b601d1df49fc9a66b2d30781e21c1d298e43f
Franco Catena <francocatena@gmail.com>
2017-01-26 12:11:30 -0300

    Support per node extra package list

--- cookbooks/packages/attributes/default.rb
+++ cookbooks/packages/attributes/default.rb
@@ -26,6 +26,9 @@ platform_specific = {
   ]
 }
 
-default['package-list'] = common + platform_specific[platform_family]
+platform_list = platform_specific[platform_family]
+node_list     = node['packages']['list'] && node['packages']['list'][platform_family]
+
+default['package-list'] = common + platform_list + Array(node_list)
 
 default['nodejs']['install_method'] = 'binary'
