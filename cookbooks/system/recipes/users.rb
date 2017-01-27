5c0bd903bb77a44602c8829914ec8c5d409f6cf9
Franco Catena <francocatena@gmail.com>
2017-01-26 21:56:45 -0300

    Dotfiles for users

--- cookbooks/system/recipes/users.rb
+++ cookbooks/system/recipes/users.rb
@@ -6,17 +6,44 @@ search(:users, "*:*").each do |user_data|
     shell       user_data['shell']
     comment     user_data['comment']
     home        home
+    notifies    :reload, 'ohai[reload]', :immediately
     manage_home true
   end
 
-  directory "#{home}/.ssh" do
-    mode  '0700'
-    owner login
+  ohai 'reload' do
+    action :nothing
+  end
+
+  group = node['etc']['passwd'][login]['gid']
+
+  %w(.ssh .vim .vim/backup .vim/undodir).each do |dir|
+    directory "#{home}/#{dir}" do
+      owner login
+      group group
+      mode  '0700'
+    end
   end
 
   file "#{home}/.ssh/authorized_keys" do
-    mode    '0600'
     owner   login
+    group   group
+    mode    '0600'
     content user_data['ssh_keys'].join("\n")
+  end
+
+  git "#{home}/.oh-my-zsh" do
+    repository 'https://github.com/robbyrussell/oh-my-zsh.git'
+    user       login
+    group      group
+    action     :checkout
+  end
+
+  %w(gemrc irbrc tmux.conf vimrc zshrc).each do |dotfile|
+    cookbook_file "#{home}/.#{dotfile}" do
+      source "dotfiles/#{dotfile}"
+      owner  login
+      group  group
+      mode   '0644'
+    end
   end
 end
