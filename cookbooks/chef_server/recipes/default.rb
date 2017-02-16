21947576618fa26f909b7fd3856acb35d75d384f
Franco Catena <francocatena@gmail.com>
2017-02-16 10:50:16 -0300

    Chef server can now _live_ with web role

--- cookbooks/chef_server/recipes/default.rb
+++ cookbooks/chef_server/recipes/default.rb
@@ -1,6 +1,22 @@
+config_options = [
+  "api_fqdn '#{node['fqdn']}'",
+  "nginx['ssl_certificate']     = '/etc/ssl/certs/#{node['fqdn']}.bundle-crt'",
+  "nginx['ssl_certificate_key'] = '/etc/ssl/private/#{node['fqdn']}.key'",
+]
+
+if node['roles'].include?('web')
+  config_options << "nginx['non_ssl_port'] = 8080"
+  config_options << "nginx['ssl_port']     = 8443"
+end
+
 chef_ingredient 'chef-server' do
-  action [:upgrade, :reconfigure]
+  action  [:upgrade, :reconfigure]
   version :latest
+  config  config_options.join("\n")
+end
+
+ingredient_config 'chef-server' do
+  notifies :reconfigure, 'chef_ingredient[chef-server]'
 end
 
 chef_ingredient 'manage' do
