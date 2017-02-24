94c225930452d2b5e69acf6f8bfe95a9883cd877
Franco Catena <francocatena@gmail.com>
2017-02-23 21:46:12 -0300

    Chef client cookbook birth

---
+++ cookbooks/chef_client/attributes/default.rb
@@ -0,0 +1,2 @@
+default['chef_client']['interval'] = 6 * 60 * 60 # every 6 hours
+default['chef_client']['splay']    = 20 * 60 # plus _randomized_ 20 minutes
---
+++ cookbooks/chef_client/metadata.rb
@@ -0,0 +1,20 @@
+name 'chef_client'
+maintainer 'Franco Catena'
+maintainer_email 'francocatena@gmail.com'
+license 'mit'
+description 'Installs/Configures chef_client'
+long_description 'Installs/Configures chef_client'
+depends 'chef-client'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/chef_client/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/chef_client' if respond_to?(:source_url)
