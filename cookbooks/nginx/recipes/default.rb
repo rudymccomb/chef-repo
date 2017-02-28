2892124413a1b4d62c742df44f849585e07f3881
Franco Catena <francocatena@gmail.com>
2017-02-28 11:46:09 -0300

    Support per site template on nginx cookbook

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -17,7 +17,7 @@ sites.each do |site|
   nginx_site site['name'] do
     template  is_production ? 'vhost_ssl.erb' : 'vhost.erb'
     variables site:               site['name'],
-              template:           node['nginx']['template'],
+              template:           site['template'] || node['nginx']['template'],
               www_dir:            node['nginx']['www_dir'],
               sock:               node['nginx']['sock_path'],
               long_proxy_timeout: node['nginx']['long_proxy_timeout']
