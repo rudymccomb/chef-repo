1be307a4e409c60431fc498e868017764d6c823b
Franco Catena <francocatena@gmail.com>
2017-02-28 11:19:54 -0300

    Better template oranization on nginx cookbook
    Add chef server template to nginx cookbook

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -1,7 +1,18 @@
+default['nginx']['template'] = 'rack'
+
+sock = case node['nginx']['template']
+       when 'rack'
+        '/run/unicorn/unicorn.sock'
+       when 'php'
+         '/var/run/php-fpm.sock'
+       when 'chef'
+       else
+         Chef::Application.fatal! 'Unsupported nginx template!'
+       end
+
 default['nginx']['default_site_enabled'] = false
 default['nginx']['www_dir']              = '/var/www'
-default['nginx']['template']             = 'rack'
-default['nginx']['sock_path']            = node['nginx']['template'] == 'rack' ? '/run/unicorn/unicorn.sock' : '/var/run/php-fpm.sock'
+default['nginx']['sock_path']            = sock
 default['nginx']['long_proxy_timeout']   = false
 
 default['openssl']['restart_services']   = ['nginx']
