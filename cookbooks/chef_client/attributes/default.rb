5ea154e456da37b269baf47f52ca1bf07939b8b7
Franco Catena <francocatena@gmail.com>
2017-02-28 18:47:04 -0300

    Longer interval on chef server client updates

--- cookbooks/chef_client/attributes/default.rb
+++ cookbooks/chef_client/attributes/default.rb
@@ -1,2 +1,4 @@
-default['chef_client']['interval'] = 6 * 60 * 60 # every 6 hours
+interval = node['roles'].include?('chef_server') ? 6 * 60 * 60 : 2 * 60 * 60
+
+default['chef_client']['interval'] = interval
 default['chef_client']['splay']    = 20 * 60 # plus _randomized_ 20 minutes
