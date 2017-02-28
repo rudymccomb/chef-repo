5c8bde7c944817b6f0325418e4da0fd9f6e5b559
Franco Catena <francocatena@gmail.com>
2017-02-28 20:28:15 -0300

    No more platform compatibility on reporting

--- cookbooks/chef_server/recipes/reporting.rb
+++ cookbooks/chef_server/recipes/reporting.rb
@@ -1,9 +1,8 @@
 chef_server = "https://#{node['fqdn']}"
 
 chef_ingredient 'reporting' do
-  action                              :upgrade
-  platform_version_compatibility_mode true
-  notifies                            :reconfigure, 'chef_ingredient[chef-server]', :immediately
+  action   :upgrade
+  notifies :reconfigure, 'chef_ingredient[chef-server]', :immediately
 end
 
 chef_ingredient 'reporting' do
