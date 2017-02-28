76217383dcb80560dfc3d8f458ad3ad46265e91a
Franco Catena <francocatena@gmail.com>
2017-02-28 20:27:08 -0300

    Allow alt names on ssl sites

--- cookbooks/ssl/recipes/default.rb
+++ cookbooks/ssl/recipes/default.rb
@@ -2,10 +2,11 @@ include_recipe 'acme'
 include_recipe 'ssl::directories'
 
 Array(node['ssl']['sites']).each do |site|
-  acme_certificate site do
-    cn        site
-    key       "#{node['ssl']['key_path']}/#{site}.key"
-    fullchain "#{node['ssl']['crt_path']}/#{site}.bundle-crt"
+  acme_certificate site['cn'] do
+    cn        site['cn']
+    alt_names Array(site['alt_names'])
+    key       "#{node['ssl']['key_path']}/#{site['cn']}.key"
+    fullchain "#{node['ssl']['crt_path']}/#{site['cn']}.bundle-crt"
     method    'http'
     wwwroot   node['ssl']['wwwroot']
     notifies  :reload, 'service[nginx]', :immediately
