a3fffe57885bd4722f1002b50c0ba4729ad4958d
Franco Catena <francocatena@gmail.com>
2017-03-01 16:43:08 -0300

    Use properly site hash on ssl::selfsigned recipe

--- cookbooks/ssl/recipes/selfsigned.rb
+++ cookbooks/ssl/recipes/selfsigned.rb
@@ -2,19 +2,19 @@ include_recipe 'acme'
 include_recipe 'ssl::directories'
 
 Array(node['ssl']['sites']).each do |site|
-  ctr_root = "#{node['ssl']['crt_path']}/#{site}"
+  ctr_root = "#{node['ssl']['crt_path']}/#{site['cn']}"
 
-  acme_selfsigned site do
-    key      "#{node['ssl']['key_path']}/#{site}.key"
-    crt      "#{ctr_root}.crt"
-    chain    "#{ctr_root}.pem"
-    notifies :run, 'execute[concat]', :immediately
-  end
-
-  execute 'concat' do
+  execute "concat_#{site['cn']}" do
     action   :nothing
     command  "cat #{ctr_root}.crt #{ctr_root}.pem > #{ctr_root}.bundle-crt"
     creates  "#{ctr_root}.bundle-crt"
     notifies :reload, 'service[nginx]', :immediately
+  end
+
+  acme_selfsigned site['cn'] do
+    key      "#{node['ssl']['key_path']}/#{site['cn']}.key"
+    crt      "#{ctr_root}.crt"
+    chain    "#{ctr_root}.pem"
+    notifies :run, "execute[concat_#{site['cn']}]", :immediately
   end
 end
