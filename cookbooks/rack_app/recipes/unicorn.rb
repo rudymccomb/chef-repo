d5a4a891e649ead83fd645da3bb5dc654480f1e2
Franco Catena <francocatena@gmail.com>
2017-03-01 16:42:25 -0300

    Depend unicorn service on redis only if recipe

--- cookbooks/rack_app/recipes/unicorn.rb
+++ cookbooks/rack_app/recipes/unicorn.rb
@@ -3,6 +3,10 @@ units_path    = value_for_platform_family(
   'rhel'    => '/usr/lib/systemd/system',
   'default' => '/lib/systemd/system'
 )
+requires      = value_for_platform_family(
+  'debian'  => node.recipe?('rack_app::sidekiq') ? ['redis-server.service'] : [],
+  'default' => node.recipe?('rack_app::sidekiq') ? ['redis.service']        : []
+)
 
 execute 'systemctl' do
   command 'systemctl daemon-reload'
@@ -15,12 +19,12 @@ template "#{units_path}/unicorn.service" do
   group     'root'
   mode      '0644'
   notifies  :run, 'execute[systemctl]', :immediately
-  variables platform_family: node['platform_family'],
-            user:            node['rack_app']['user'],
-            group:           node['rack_app']['group'],
-            base_dir:        node['rack_app']['base_dir'],
-            rbenv_root:      node['rack_app']['rbenv_path'],
-            is_production:   is_production
+  variables user:          node['rack_app']['user'],
+            group:         node['rack_app']['group'],
+            base_dir:      node['rack_app']['base_dir'],
+            rbenv_root:    node['rack_app']['rbenv_path'],
+            requires:      requires,
+            is_production: is_production
 end
 
 service 'unicorn' do
