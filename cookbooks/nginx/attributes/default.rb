a14d1a9d18abe04befd9aedec3bf59a114090834
Franco Catena <francocatena@gmail.com>
2017-03-01 16:38:02 -0300

    Support per site sock option on nginx cookbook

--- cookbooks/nginx/attributes/default.rb
+++ cookbooks/nginx/attributes/default.rb
@@ -1,18 +1,6 @@
-default['nginx']['template'] = 'rack'
-
-sock = case node['nginx']['template']
-       when 'rack'
-        '/run/unicorn/unicorn.sock'
-       when 'php'
-         '/var/run/php-fpm.sock'
-       when 'chef'
-       else
-         Chef::Application.fatal! 'Unsupported nginx template!'
-       end
-
 default['nginx']['default_site_enabled'] = false
+default['nginx']['template']             = 'rack'
 default['nginx']['www_dir']              = '/var/www'
-default['nginx']['sock_path']            = sock
 default['nginx']['long_proxy_timeout']   = false
 
 default['openssl']['restart_services']   = ['nginx']
--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -4,6 +4,10 @@ include_recipe 'openssl::upgrade'
 sites         = Array node['nginx']['sites'] || [{ 'name' => node['fqdn'] }]
 config_dir    = node['nginx']['dir']
 is_production = node['virtualization']['system'] != 'vbox'
+default_sock  = {
+  'rack' =>  '/run/unicorn/unicorn.sock',
+  'php'  =>  '/var/run/php-fpm.sock'
+}
 
 if is_production
   openssl_dhparam "#{config_dir}/dhparams.pem" do
@@ -13,13 +17,17 @@ if is_production
   end
 end
 
-sites.each do |site|
+sites.each_with_index do |site, i|
+  template = site['template'] || node['nginx']['template']
+  sock     = site['sock']     || default_sock[template]
+
   nginx_site site['name'] do
     template  is_production ? 'vhost_ssl.erb' : 'vhost.erb'
     variables site:               site['name'],
-              template:           site['template'] || node['nginx']['template'],
               www_dir:            node['nginx']['www_dir'],
-              sock:               node['nginx']['sock_path'],
-              long_proxy_timeout: node['nginx']['long_proxy_timeout']
+              long_proxy_timeout: node['nginx']['long_proxy_timeout'],
+              template:           template,
+              sock:               sock,
+              deferred:           i == 0
   end
 end
