40af11f943aa607b406ce656209fff2261a0fe62
Franco Catena <francocatena@gmail.com>
2017-03-02 00:46:42 -0300

    Fix permissions on key path dir on ssl cookbook

--- cookbooks/ssl/recipes/directories.rb
+++ cookbooks/ssl/recipes/directories.rb
@@ -1,3 +1,8 @@
+key_path_group = value_for_platform_family(
+  'debian'  => 'ssl-cert',
+  'default' => 'root'
+)
+
 directory node['ssl']['crt_path'] do
   owner 'root'
   group 'root'
@@ -6,7 +11,7 @@ end
 
 directory node['ssl']['key_path'] do
   owner 'root'
-  group 'root'
-  mode  '0700'
+  group key_path_group
+  mode  '0710'
 end
 
