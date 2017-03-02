6ec7259a612120af2cd0c09dd74a807aeccc8597
Franco Catena <francocatena@gmail.com>
2017-03-02 00:55:01 -0300

    Only requite prefix on SSL alt names

--- cookbooks/ssl/recipes/default.rb
+++ cookbooks/ssl/recipes/default.rb
@@ -2,9 +2,13 @@ include_recipe 'acme'
 include_recipe 'ssl::directories'
 
 Array(node['ssl']['sites']).each do |site|
+  alt_names = Array(site['alt_names']).map do |prefix|
+    "#{prefix}.#{site['cn']}"
+  end
+
   acme_certificate site['cn'] do
     cn        site['cn']
-    alt_names Array(site['alt_names'])
+    alt_names alt_names
     key       "#{node['ssl']['key_path']}/#{site['cn']}.key"
     fullchain "#{node['ssl']['crt_path']}/#{site['cn']}.bundle-crt"
     method    'http'
