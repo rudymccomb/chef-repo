3cd1346175516f563565cb044f8a27f1b46956c3
Franco Catena <francocatena@gmail.com>
2017-03-09 19:03:39 -0300

    Ruby 2.4.0

--- cookbooks/rack_app/attributes/default.rb
+++ cookbooks/rack_app/attributes/default.rb
@@ -1,4 +1,4 @@
-default['rack_app']['ruby_version'] = '2.3.3'
+default['rack_app']['ruby_version'] = '2.4.0'
 default['rack_app']['user']         = 'deployer'
 default['rack_app']['group']        = node['platform_family'] == 'rhel' ? 'nginx' : 'www-data'
 default['rack_app']['base_dir']     = '/var/www/app'
