024bb45c079b5d997bede22acc5c628ad31c9df7
Franco Catena <francocatena@gmail.com>
2017-03-22 11:32:04 -0300

    Make php fpm user a configuration

--- cookbooks/php_app/attributes/default.rb
+++ cookbooks/php_app/attributes/default.rb
@@ -1,9 +1,2 @@
 default['php_app']['listen']       = '/var/run/php-fpm.sock'
 default['php_app']['use_sendmail'] = false
-default['php_app']['user']         = if node['etc']['passwd']['deployer']
-                                       'deployer'
-                                     elsif node.platform_family? 'debian'
-                                       'www-data'
-                                     else
-                                       'nginx'
-                                     end
--- cookbooks/php_app/recipes/default.rb
+++ cookbooks/php_app/recipes/default.rb
@@ -1,7 +1,12 @@
 include_recipe 'php_app::sendmail' if node['php_app']['sendmail']
 
+fpm_user = node['php_app']['user'] || value_for_platform_family(
+  'debian'  => 'www-data',
+  'default' => 'nginx'
+)
+
 php_fpm_pool 'default' do
-  user   node['php_all']['user']
+  user   fpm_user
   listen node['php_app']['listen']
 end
 
