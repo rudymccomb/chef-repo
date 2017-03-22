2ac12f0bb081c0bdac779d1ef64c1fa7c4a362dc
Franco Catena <francocatena@gmail.com>
2017-03-22 18:27:13 -0300

    Make rbenv upgrade at user level

--- cookbooks/rack_app/attributes/default.rb
+++ cookbooks/rack_app/attributes/default.rb
@@ -6,10 +6,12 @@ default['rack_app']['rbenv_path']   = "/home/#{node['rack_app']['user']}/.rbenv"
 
 default['rbenv']['user_installs'] = [
   {
-    'user'   => node['rack_app']['user'],
-    'rubies' => [node['rack_app']['ruby_version']],
-    'global' => node['rack_app']['ruby_version'],
-    'gems'   => {
+    'user'    => node['rack_app']['user'],
+    'rubies'  => [node['rack_app']['ruby_version']],
+    'global'  => node['rack_app']['ruby_version'],
+    'git_ref' => node['rack_app']['rbenv_git_ref'] || 'master',
+    'upgrade' => true,
+    'gems'    => {
       node['rack_app']['ruby_version'] => [
         { 'name' => 'bundler' }
       ]
--- cookbooks/rack_app/attributes/rbenv.rb
+++ cookbooks/rack_app/attributes/rbenv.rb
@@ -1,4 +1,1 @@
-default['ruby_rbenv']['git_ref'] = 'master'
-default['ruby_rbenv']['upgrade'] = true
-
 default['ruby_build']['upgrade'] = true
