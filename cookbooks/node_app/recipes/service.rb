2ba91d2f6a5e180c1b3c01fce049b953ea2d4c32
Franco Catena <francocatena@gmail.com>
2017-03-31 11:45:51 -0300

    Allow multiple node apps on the same server

--- cookbooks/node_app/recipes/service.rb
+++ cookbooks/node_app/recipes/service.rb
@@ -8,21 +8,23 @@ execute 'systemctl' do
   action  :nothing
 end
 
-template "#{units_path}/node.service" do
-  source    'node.service.erb'
-  owner     'root'
-  group     'root'
-  mode      '0644'
-  notifies  :run, 'execute[systemctl]', :immediately
-  variables user:       node['node_app']['user'],
-            group:      node['node_app']['group'],
-            base_dir:   node['node_app']['base_dir'],
-            node_path:  node['node_app']['node_path'],
-            entrypoint: node['node_app']['entrypoint'],
-            env:        Array(node['node_app']['env']),
-            requires:   Array(node['node_app']['requires'])
-end
+Array(node['node_app']['apps']).each do |app|
+  template "#{units_path}/#{app['name']}.service" do
+    source    'node.service.erb'
+    owner     'root'
+    group     'root'
+    mode      '0644'
+    notifies  :run, 'execute[systemctl]', :immediately
+    variables user:       app['user']       || node['node_app']['user'],
+              group:      app['group']      || node['node_app']['group'],
+              base_dir:   app['base_dir']   || node['node_app']['base_dir'],
+              entrypoint: app['entrypoint'] || node['node_app']['entrypoint'],
+              node_path:  node['node_app']['node_path'],
+              env:        Array(app['env']      || node['node_app']['env']),
+              requires:   Array(app['requires'] || node['node_app']['requires'])
+  end
 
-service 'node' do
-  action :enable
+  service app['name'] do
+    action :enable
+  end
 end
