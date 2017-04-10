b39d1f04613a1c235fa53f4adabefe740f3c4a32
Franco Catena <francocatena@gmail.com>
2017-04-10 01:50:12 -0300

    Allow all users to login on PostgreSQL

--- cookbooks/postgres/attributes/default.rb
+++ cookbooks/postgres/attributes/default.rb
@@ -8,8 +8,8 @@ default['postgresql']['password']['postgres'] = 'postgres'
 default['postgresql']['pg_hba'] = [
   { type: 'local', db: 'all', user: 'postgres', addr: nil,            method: 'ident' },
   { type: 'local', db: 'all', user: 'all',      addr: nil,            method: 'md5' },
-  { type: 'host',  db: 'all', user: 'postgres', addr: '127.0.0.1/32', method: 'md5' },
-  { type: 'host',  db: 'all', user: 'postgres', addr: '::1/128',      method: 'md5' }
+  { type: 'host',  db: 'all', user: 'all',      addr: '127.0.0.1/32', method: 'md5' },
+  { type: 'host',  db: 'all', user: 'all',      addr: '::1/128',      method: 'md5' }
 ]
 
 case node['platform_family']
