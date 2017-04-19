d36a64409a4a3bbf26494eb2e3845bd110d7056a
Franco Catena <francocatena@gmail.com>
2017-04-19 16:35:49 -0300

    Let's be explicit about ssl renewals

--- cookbooks/ssl/attributes/default.rb
+++ cookbooks/ssl/attributes/default.rb
@@ -3,5 +3,6 @@ default['ssl']['crt_path']  = '/etc/ssl/certs'
 default['ssl']['key_path']  = '/etc/ssl/private'
 default['ssl']['sites']     = [{ 'cn' => node['fqdn'] }]
 
+default['acme']['renew']    = 30
 default['acme']['contact']  = ['mailto:francocatena@gmail.com']
 default['acme']['endpoint'] = 'https://acme-v01.api.letsencrypt.org'
