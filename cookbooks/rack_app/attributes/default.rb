356b47b35f67daad7377177c2ba7df54e0887f63
Franco Catena <francocatena@gmail.com>
2017-04-25 20:06:25 -0300

    Move rbenv attributes where they belong

--- cookbooks/rack_app/attributes/default.rb
+++ cookbooks/rack_app/attributes/default.rb
@@ -3,18 +3,3 @@ default['rack_app']['user']         = 'deployer'
 default['rack_app']['group']        = node['platform_family'] == 'rhel' ? 'nginx' : 'www-data'
 default['rack_app']['base_dir']     = '/var/www/app'
 default['rack_app']['rbenv_path']   = "/home/#{node['rack_app']['user']}/.rbenv"
-
-default['rbenv']['user_installs'] = [
-  {
-    'user'      => node['rack_app']['user'],
-    'rubies'    => [node['rack_app']['ruby_version']],
-    'global'    => node['rack_app']['ruby_version'],
-    'git_ref'   => node['rack_app']['rbenv_git_ref'] || 'master',
-    'upgrade'   => true,
-    'user_gems' => {
-      node['rack_app']['ruby_version'] => [
-        { 'name' => 'bundler' }
-      ]
-    }
-  }
-]
--- cookbooks/rack_app/attributes/rbenv.rb
+++ cookbooks/rack_app/attributes/rbenv.rb
@@ -1,1 +1,16 @@
-default['ruby_build']['upgrade'] = true
+default['ruby_build']['upgrade']  = true
+
+default['rbenv']['user_installs'] = [
+  {
+    'user'      => node['rack_app']['user'],
+    'rubies'    => [node['rack_app']['ruby_version']],
+    'global'    => node['rack_app']['ruby_version'],
+    'git_ref'   => node['rack_app']['rbenv_git_ref'] || 'master',
+    'upgrade'   => true,
+    'user_gems' => {
+      node['rack_app']['ruby_version'] => [
+        { 'name' => 'bundler' }
+      ]
+    }
+  }
+]
