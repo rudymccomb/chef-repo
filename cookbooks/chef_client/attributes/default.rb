931790ebc6ea07705dee5dcc832c8983a8173bb0
Franco Catena <francocatena@gmail.com>
2017-05-08 22:33:59 -0300

    Move chef client version to attributes

--- cookbooks/chef_client/attributes/default.rb
+++ cookbooks/chef_client/attributes/default.rb
@@ -1,4 +1,5 @@
 interval = node.role?('chef_server') ? 6 * 60 * 60 : 2 * 60 * 60
 
+default['chef_client']['version']  = '12.20.3'
 default['chef_client']['interval'] = interval
 default['chef_client']['splay']    = 20 * 60 # plus _randomized_ 20 minutes
--- cookbooks/chef_client/recipes/default.rb
+++ cookbooks/chef_client/recipes/default.rb
@@ -2,6 +2,6 @@ include_recipe 'chef-client'
 
 chef_ingredient 'chef' do
   action   :upgrade
-  version  '12.20.3'
+  version  node['chef_client']['version']
   notifies :restart, 'service[chef-client]', :delayed
 end
